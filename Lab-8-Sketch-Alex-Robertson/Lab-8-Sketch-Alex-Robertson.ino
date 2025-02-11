c99fbe78d175cc56cbeda19f7e2033048ec8ed46
AlexInfi <rarobe1@ilstu.edu>
2025-02-11 15:16:39 -0600

    Traffic Light with button press
    Same simple traffic light system, but now with the option of a manual button press to turn all lights on at the same time to cause car crashes.

--- Lab-8-Sketch-Alex-Robertson/Lab-8-Sketch-Alex-Robertson.ino
+++ Lab-8-Sketch-Alex-Robertson/Lab-8-Sketch-Alex-Robertson.ino
@@ -9,18 +9,44 @@ void setup() {
 }
 
 void loop() {
-  // Red light
+
+  while (digitalRead(2) == HIGH) {
+    // Turn non-red lights off
+    digitalWrite(6, LOW);
+    digitalWrite(5, LOW);
+
+    // Checks for the button press in between each light - probably bad coding technique :)
+    if (digitalRead(2) == LOW) {
+      break;
+    }
+
+    // Red light
+    digitalWrite(7, HIGH);
+    delay(2500);
+    digitalWrite(7, LOW);
+
+    if (digitalRead(2) == LOW) {
+      break;
+    }
+
+    // Green light
+    digitalWrite(5, HIGH);
+    delay(2500);
+    digitalWrite(5, LOW);
+
+    if (digitalRead(2) == LOW) {
+      break;
+    }
+
+    // Yellow light
+    digitalWrite(6, HIGH);
+    delay(2500);
+    digitalWrite(6, LOW);
+  }
+
+  // Turns on all lights simultaneously
   digitalWrite(7, HIGH);
-  delay(5000);
-  digitalWrite(7, LOW);
-
-  // Green light
   digitalWrite(5, HIGH);
-  delay(5000);
-  digitalWrite(5, LOW);
-
-  // Yellow light
   digitalWrite(6, HIGH);
   delay(2500);
-  digitalWrite(6, LOW);
 }
